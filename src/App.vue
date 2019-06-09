cfc207b35cdf03027493d18050023d4d04b2f28a
andreas <andreas.groos1@gmail.com>
2019-06-09 09:20:32 -0700

    Working with distances

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,54 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>NC trails</h1>
+    <ol>
+      <li v-for="trail in trails" :key="trail.attributes.OBJECTID">{{trailName(trail.attributes)}}</li>
+    </ol>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import { lineString, length } from "@turf/turf";
+import { log } from "util";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  components: {},
+  data: function() {
+    return {
+      trails: []
+    };
+  },
+  mounted() {
+    const getData = async () => {
+      let temp = await axios.get(
+        "https://gis.nevcounty.net/arcgis/rest/services/web_public/Open_Data_Layers_Nevada_County/MapServer/1400/query?where=1%3D1&outFields=*&outSR=4326&f=json"
+      );
+      const allTrails = temp.data.features;
+      this.trails = allTrails.map(t => {
+        var line = lineString(t.geometry.paths[0]);
+        var l = length(line, { units: "miles" });
+        return { ...t, attributes: { ...t.attributes, length: l } };
+      });
+    };
+    getData();
+  },
+  methods: {
+    trailName: function(trail) {
+      return trail.OWNERSHIP
+        ? `${trail.NAME} - ${trail.OWNERSHIP} - ${trail.length.toFixed(1)}mi`
+        : `${trail.NAME} - ${trail.length.toFixed(1)}mi`;
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
