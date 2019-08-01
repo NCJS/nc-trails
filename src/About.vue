cd991074165975d6dad6c9ba9fba5e9e177687c8
Shlomit <shlomitgazit12@hotmail.com>
2019-08-01 16:47:10 -0700

    Adding header

---
+++ src/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="About" class="about">
+    <h1>Who we are?</h1>
+    <p>
+     This is our navada county meetup communal project
+     <br>
+     More words to come...
+    </p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'About'
+};
+</script>
