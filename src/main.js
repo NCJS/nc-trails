2eebd28f0e910e22cc1c6ab2c5f35a57a0b0114e
Shlomit <shlomitgazit12@hotmail.com>
2019-08-03 12:56:06 -0700

    issue#8

--- src/main.js
+++ src/main.js
@@ -1,12 +1,36 @@
 import Vue from "vue";
 import App from "./App.vue";
+import Home from "./Home.vue";
+import About from "./About.vue";
 import { ClientTable } from "vue-tables-2";
 import "leaflet/dist/leaflet.css";
+import VueRouter from "vue-router";
+import { hostname } from "os";
 
 Vue.use(ClientTable);
+Vue.use(VueRouter);
 
 Vue.config.productionTip = false;
 
+const routes = [
+  {
+    path:'/Home',
+    name: 'Home',
+    component:Home
+  },
+  {
+    path:'/About',
+    name: 'About',
+    component: About
+  }
+]
+
+const router = new VueRouter({
+  routes: routes,
+  mode: 'history'
+});
+
 new Vue({
+  router,
   render: h => h(App)
 }).$mount("#app");
