a705e8d6151d489d26c472cd63310daf0998f7a0
Shlomit <shlomitgazit12@hotmail.com>
2019-08-14 19:21:18 -0700

    issue#8

--- src/About.vue
+++ src/About.vue
@@ -4,7 +4,9 @@
     <p>
      This is our navada county meetup communal project
      <br>
-     More words to come...
+     If you are interested in contributing to the project, please find this link:
+     <br>
+     <a href="https://github.com/NCJS/nc-trails" target="_blank">Click here</a>
     </p>
   </div>
 </template>
@@ -15,3 +17,12 @@ export default {
   name: 'About'
 };
 </script>
+
+<style about>
+.about{
+  list-style-type: none;
+  text-align: left;
+  padding: 15px 32px;
+  font-size: large;
+}
+</style>
